03a9260a788789b45d81307b5cabb05dcd885624
YeahOrNope <148880342+YeahOrNope@users.noreply.github.com>
2023-12-19 15:01:31 +0000

    lollololo

--- src/App.svelte
+++ src/App.svelte
@@ -1,73 +1,101 @@
 <script lang="ts">
-	const boardwidth = 10;
-    const boardWeight = 10;
+    const boardWidth = 10;
+    const boardHeight = 10;
 
-    let snake: rumber[][];
+    let snake: number[][];
     let direction: [number, number];
-
+    // let food: [number, number];
     let intervalID: number | undefined;
+    // let gameOver = false;
+    // let score = 0;
 
-
-
+    // interval between each update in [ms]
     let intervalTimeout = 150;
 
     function setup() {
+        // score = 0;
         snake = [
-            [2,0],
-            [1,0],
-            [0,0],
+            [2, 0],
+            [1, 0],
+            [0, 0],
         ];
-
+        direction = [1, 0];
+        // gameOver = false;
     }
 
-setup();
+    setup();
 
-    funtion uptade() {
+    function update() {
         const [headX, headY] = snake[0];
-        const [headX + directionX, headY + directionY];
+        const [directionX, directionY] = direction;
+
+        // calculate next head position
+        const [nextX, nextY] = [headX + directionX, headY + directionY];
 
         snake.unshift([nextX, nextY]);
 
         snake.pop();
 
+        // update snake to trigger reactivity (rendering)
         snake = snake;
-        console.log("uptade");
+        console.log("update");
     }
 
-    function handleKeydown(e: KeyboardEvent){
+    function handleKeydown(e: KeyboardEvent) {
         switch (e.key) {
             case "ArrowUp":
             case "w":
-                if (snake[0][1] - snake[1][1] != 1) direction = [0,-1];
+                if (snake[0][1] - snake[1][1] !== 1) direction = [0, -1];
                 break;
-                case "ArrowDown":
-                case "s":
-                    if (snake[0][1] - snake[1][1] != 1) direction = [0,1];
-                    break;
-                case "ArrowLeft":
-                case "a":
-                    if (snake[0][1] - snake[1][1] != 1) direction = [-1,0];
-                    break;
-                case "ArrowRight":
-                case "d":
-                    if (snake[0][1] - snake[1][1] != 1) direction = [1,0];
-                    break;
+            case "ArrowDown":
+            case "s":
+                if (snake[0][1] - snake[1][1] !== -1) direction = [0, 1];
+                break;
+            case "ArrowLeft":
+            case "a":
+                if (snake[0][0] - snake[1][0] !== 1) direction = [-1, 0];
+                break;
+            case "ArrowRight":
+            case "d":
+                if (snake[0][0] - snake[1][0] !== -1) direction = [1, 0];
+                break;
         }
     }
 
-    setInterval(uptade, internalTimeout);
+    setInterval(update, intervalTimeout);
 </script>
 
-<main>
-    {#each Array (boardHeight).fill(0) as _,y}
-        <div class="row">
-            {#each Array(boardWidth).fill(0) as _,x}
-                <div class="cell {snake}.find([sx, sy])=> sx === x && sy === y) ? 'snake' : '' }">
+<!-- globally capture keydown events -->
+<svelte:window on:keydown={handleKeydown} />
 
-                </div>
+<main>
+    {#each Array(boardHeight).fill(0) as _, y}
+        <div class="row">
+            {#each Array(boardWidth).fill(0) as _, x}
+                <!-- [{x}, {y}] -->
+                <div
+                    class="cell
+                {snake.find(([sx, sy]) => sx === x && sy === y) ? 'snake' : ''} 
+                "
+                ></div>
+            {/each}
         </div>
+    {/each}
 </main>
 
 <style>
-    /* Styl strony */
+    .row {
+        display: flex;
+    }
+    .cell {
+        width: 40px;
+        height: 40px;
+        border: 2px solid gray;
+        border-radius: 15%;
+        background-color: greenyellow;
+        margin: 1px;
+    }
+    .snake {
+        background-color: slateblue;
+    }
 </style>
